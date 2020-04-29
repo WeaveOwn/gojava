e236a72769d560886859ce73845901613b78b417
wangwei <wangwei@kingxunlian.com>
2020-04-29 23:17:14 +0800

    single

--- src/main/java/com/weaveown/design/create/singleton/SingletonHolderDemo.java
+++ src/main/java/com/weaveown/design/create/singleton/SingletonHolderDemo.java
@@ -9,9 +9,21 @@ public class SingletonHolderDemo {
      * 静态内部类单例
      */
     private static class SingletonHolder{
+        static {
+            System.out.println("被调用");
+        }
         private final static SingletonHolderDemo DEMO = new SingletonHolderDemo();
     }
-    public SingletonHolderDemo getInstance() {
+    public static SingletonHolderDemo getInstance() {
         return SingletonHolder.DEMO;
+    }
+
+    private SingletonHolderDemo() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("start");
+        SingletonHolderDemo singletonHolderDemo = SingletonHolderDemo.getInstance();
     }
 }
